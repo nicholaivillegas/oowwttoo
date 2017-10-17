b56c52967d92f5957097a96c068259d952c6162d
MYNT\john.villegas <john.villegas@mynt.xyz>
2017-10-17 08:51:38 +0800

    UI

--- app/src/main/java/com/villegapps/owto/MainActivity.java
+++ app/src/main/java/com/villegapps/owto/MainActivity.java
@@ -1,7 +1,16 @@
 package com.villegapps.owto;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
@@ -14,18 +23,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double currentLatitude, currentLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        getLocation();
     }
 
     @Override
@@ -89,13 +103,13 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText(this, "Payment", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_history) {
             Toast.makeText(this, "History", Toast.LENGTH_SHORT).show();
-        }else if (id == R.id.nav_schedule) {
+        } else if (id == R.id.nav_schedule) {
             Toast.makeText(this, "Schedule", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_notifications) {
             Toast.makeText(this, "Notifications", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_emergency) {
             Toast.makeText(this, "Emergency", Toast.LENGTH_SHORT).show();
-        }else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_share) {
             Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_help) {
             Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
@@ -119,11 +133,57 @@ public class MainActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng charbel = new LatLng(14.680589, 121.031829);
-        mMap.addMarker(new MarkerOptions().position(charbel).title("Marker in St. Charbel"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(charbel));
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(17.0f);
+//        LatLng charbel = new LatLng(14.680589, 121.031829);
+//        mMap.addMarker(new MarkerOptions().position(charbel).title("Marker in St. Charbel"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(charbel));
+//        CameraUpdate zoom = CameraUpdateFactory.zoomTo(17.0f);
+//        mMap.animateCamera(zoom);
+        mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                LatLng center = mMap.getCameraPosition().target;
+                try {
+                    Toast.makeText(MainActivity.this, getAddress(center.latitude, center.longitude), Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude, currentLongitude), 17.0f));
+    }
 
-        mMap.animateCamera(zoom);
+    public void getLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+    }
+
+    public String getAddress(double latitude, double longitude) throws IOException {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String country = addresses.get(0).getCountryName();
+        String postalCode = addresses.get(0).getPostalCode();
+        String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+
+        return address;
     }
 }
